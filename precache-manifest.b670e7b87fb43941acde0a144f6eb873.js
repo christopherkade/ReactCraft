ada76b497a7ea7a6a3626e56c871f81c42467ce5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-17 14:12:33 +0000

    Deploy christopherkade/ReactCraft to github.com/christopherkade/ReactCraft.git:gh-pages

---
+++ precache-manifest.b670e7b87fb43941acde0a144f6eb873.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d0625f867ee67dd3e49cd79dfc0bd2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3bb8743edbd9c7aef2ac.bundle.js"
+  },
+  {
+    "url": "runtime~main.3bb8743edbd9c7aef2ac.bundle.js"
+  },
+  {
+    "revision": "8eccce155090104c7902b964cd0e12fe",
+    "url": "static/media/FrizQuadrataTT.8eccce15.ttf"
+  },
+  {
+    "url": "vendors~main.3bb8743edbd9c7aef2ac.bundle.js"
+  }
+]);
